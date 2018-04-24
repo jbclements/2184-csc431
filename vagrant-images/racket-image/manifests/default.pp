a5647627dd82628f30aead524483d66bfc131e81
John Clements <clements@racket-lang.org>
2018-04-23 17:35:20 -0700

    working on vms...

--- vagrant-images/racket-image/manifests/default.pp
+++ vagrant-images/racket-image/manifests/default.pp
@@ -1,13 +1,13 @@
-    include apt
-    
-    class { 'apt':
-     always_apt_update => true,
-    }
-    
-    apt::ppa { 'ppa:plt/racket':}
-    
-    package { "racket":
-      ensure => 'installed',
-      require => Apt::Ppa['ppa:plt/racket'],
-    }
+include apt
+
+class { 'apt':
+ always_apt_update => true,
+}
+
+apt::ppa { 'ppa:plt/racket':}
+
+package { "racket":
+  ensure => 'installed',
+  require => Apt::Ppa['ppa:plt/racket'],
+}
 
